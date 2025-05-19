b1e1b4aa92ed37f9bff82ee4aed92f8deacf8e3f
zorigto-sambuev <muraways@gmail.com>
2025-05-19 16:48:24 -0600

    updated beforeEach in overlays, news specs

--- tests/main/overlays.spec.ts
+++ tests/main/overlays.spec.ts
@@ -1,14 +1,18 @@
-import { test, expect } from '@playwright/test';
+import { test } from '../fixtures';
+import { expect } from '@playwright/test';
 
 test.describe('Main Page Overlays Tests', () => {
     test.beforeEach(async ({ page }) => {
+        await page.goto(process.env.BASE_URL!);
         const adModal = page.getByTestId('interstitial-ad-modal');
         const closeButton = page.getByTestId('interstitial-ad-close-button');
-    
-        await page.goto(process.env.BASE_URL!);
-        if (await adModal.isVisible()) {
-          await closeButton.click();
-          await expect(adModal).not.toBeVisible();
+        try {
+            await expect(adModal).toBeVisible({ timeout: 3000 });
+            await expect(closeButton).toBeVisible({ timeout: 3000 });
+            await closeButton.click();
+            await expect(adModal).not.toBeVisible({ timeout: 3000 });
+        } catch (error) {
+            console.error('Error closing ad modal:');
         }
     });
 
